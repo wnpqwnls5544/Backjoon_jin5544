3161098271be4297c54b939d6f7dcc2e5b70fd5c
wnpqwnls5544 <52951664+wnpqwnls5544@users.noreply.github.com>
2022-09-25 15:46:06 +0900

    Create num_10818.java

---
+++ num_10818.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            else if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
